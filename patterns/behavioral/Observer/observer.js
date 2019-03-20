1194166e390a0addc7cb81398d1906e6fc2221c0
Denys Sidorov <000scorpions000@gmail.com>
2019-03-20 18:03:21 +0200

    create another Observer implementation

---
+++ patterns/behavioral/Observer/observer.js
@@ -0,0 +1,53 @@
+/**
+ * 'Observer / Publisher-Subscriber' pattern allows you have one publisher and many another
+ * subscribers
+ */
+
+// todo encapsulation
+// todo tests
+// todo chaining
+function YoutubeChannel(name) {
+  this.name = name;
+  this.subscribers = []
+}
+
+YoutubeChannel.prototype.subscribe = function (subscriber) {
+  this.subscribers.push(subscriber);
+}
+
+YoutubeChannel.prototype.unsubscribe = function (subscriber) {
+  this.subscribers = this.subscribers.filter((item) => item.id !== subscriber.id);
+}
+
+YoutubeChannel.prototype.reset = function () {
+  this.subscribers = [];
+}
+
+YoutubeChannel.prototype.publishNews = function (news) {
+  this.subscribers.forEach(user => user.readNews(news));
+}
+
+
+// clients:
+var User1 = {
+  id: 1,
+  name: 'Denis',
+  readNews: function (news) {
+    console.log(`${this.name} got fresh news: ${news}`);
+  }
+}
+
+var User2 = {
+  id: 2,
+  name: 'Marry',
+  readNews: function (news) {
+    console.log(`${this.name} got fresh news: ${news}`);
+  }
+}
+
+var channel = new YoutubeChannel('bo');
+
+channel.subscribe(User1);
+channel.subscribe(User2);
+
+channel.publishNews('We have 1m users!');
