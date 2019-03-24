2598fbf21a8c55facd3331b25de0a11d068929a4
Denys Sidorov <000scorpions000@gmail.com>
2019-03-24 13:09:21 +0200

    add tests for Chain pattern, remove comments

--- patterns/behavioral/Chain_Of_Responsibility/chain.js
+++ patterns/behavioral/Chain_Of_Responsibility/chain.js
@@ -3,30 +3,6 @@
  * it can be used in long chain of calls
  */
 
-
-// function CarSkeleton(name) {
-//   this.name = name;
-//   this.parts = [];
-// }
-//
-// CarSkeleton.prototype.addPart = function (part) {
-//   this.parts.push(part);
-//   return this;
-// }
-//
-// CarSkeleton.prototype.getCount = function () {
-//   return this.parts.length;
-//
-// }
-//
-// var rover = new CarSkeleton('Rover');
-//
-// rover.addPart('door').addPart('cover').addPart('chairs').getCount();
-//
-// console.log(rover.getCount());
-
-
-
 function CarSkeleton(name) {
   let _name = name;
   let _parts = [];
