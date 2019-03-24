4dfb12028ee9aa5306dfb299b7a80918b925ba64
Denys Sidorov <000scorpions000@gmail.com>
2019-03-24 14:06:31 +0200

    start creating Builder pattern

---
+++ patterns/creational/Builder/builder.js
@@ -0,0 +1,131 @@
+/**
+ * Factory
+ * Builder car
+ * Car
+ * */
+
+// interface
+function Factory() {
+  this.build = function (builder) {
+    builder.step1();
+    builder.step2();
+    return builder.getCar();
+  }
+}
+
+// interface
+function BuilderX5(color) {
+  this.car = null;
+  this.step1 = function () {
+    this.car = new BMW_X5();
+  }
+  this.step2 = function () {
+    this.car.paint(color);
+  }
+  this.getCar = function () {
+    return this.car;
+  }
+}
+
+// class implementation
+function BMW_X5(color) {
+  var car = {color: 'white', /** Another properties*/};
+  this.paint = function () {
+    car.color =  'color';
+  }
+}
+
+var factory = new Factory();
+var bmwX5Builder = new BMW_X5('red');
+var car = factory.build(bmwX5Builder);
+console.log(car);
+
+
+
+
+function Shop() {
+  this.construct = function(builder) {
+    builder.step1();
+    builder.step2();
+    return builder.get();
+  }
+}
+
+function CarBuilder() {
+  this.car = null;
+
+  this.step1 = function() {
+    this.car = new Car();
+  };
+
+  this.step2 = function() {
+    this.car.addParts();
+  };
+
+  this.get = function() {
+    return this.car;
+  };
+}
+
+function TruckBuilder() {
+  this.truck = null;
+
+  this.step1 = function() {
+    this.truck = new Truck();
+  };
+
+  this.step2 = function() {
+    this.truck.addParts();
+  };
+
+  this.get = function() {
+    return this.truck;
+  };
+}
+
+function Car() {
+  this.doors = 0;
+
+  this.addParts = function() {
+    this.doors = 4;
+  };
+
+  this.say = function() {
+    log.add("I am a " + this.doors + "-door car");
+  };
+}
+
+function Truck() {
+  this.doors = 0;
+
+  this.addParts = function() {
+    this.doors = 2;
+  };
+
+  this.say = function() {
+    log.add("I am a " + this.doors + "-door truck");
+  };
+}
+
+// log helper
+var log = (function () {
+  var log = "";
+  return {
+    add: function (msg) { log += msg + "\n"; },
+    show: function () { console.log(log); log = ""; }
+  }
+})();
+
+function run() {
+  var shop = new Shop();
+  var carBuilder = new CarBuilder();
+  var truckBuilder = new TruckBuilder();
+  var car = shop.construct(carBuilder);
+  var truck = shop.construct(truckBuilder);
+
+  car.say();
+  truck.say();
+
+  log.show();
+}
+
