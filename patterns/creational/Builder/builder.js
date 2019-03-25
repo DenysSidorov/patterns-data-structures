62551b25b339236cad313a372d993e302f3451ab
Denys Sidorov <000scorpions000@gmail.com>
2019-03-25 17:38:24 +0200

    create Builder pattern

--- patterns/creational/Builder/builder.js
+++ patterns/creational/Builder/builder.js
@@ -6,6 +6,7 @@
 
 // interface
 function Factory() {
+
   this.build = function (builder) {
     builder.step1();
     builder.step2();
@@ -28,104 +29,106 @@ function BuilderX5(color) {
 }
 
 // class implementation
-function BMW_X5(color) {
-  var car = {color: 'white', /** Another properties*/};
-  this.paint = function () {
-    car.color =  'color';
+function BMW_X5() {
+  this.props = {color: 'white', /** Another properties*/};
+  this.paint = function (color) {
+    this.props.color =  color;
   }
 }
 
 var factory = new Factory();
-var bmwX5Builder = new BMW_X5('red');
-var car = factory.build(bmwX5Builder);
-console.log(car);
+var BMWBuilder = new BuilderX5('red');
+var car = factory.build(BMWBuilder);
+
+
+console.log(car.props);
 
 
 
 
-function Shop() {
-  this.construct = function(builder) {
-    builder.step1();
-    builder.step2();
-    return builder.get();
-  }
-}
-
-function CarBuilder() {
-  this.car = null;
-
-  this.step1 = function() {
-    this.car = new Car();
-  };
-
-  this.step2 = function() {
-    this.car.addParts();
-  };
-
-  this.get = function() {
-    return this.car;
-  };
-}
-
-function TruckBuilder() {
-  this.truck = null;
-
-  this.step1 = function() {
-    this.truck = new Truck();
-  };
-
-  this.step2 = function() {
-    this.truck.addParts();
-  };
-
-  this.get = function() {
-    return this.truck;
-  };
-}
-
-function Car() {
-  this.doors = 0;
-
-  this.addParts = function() {
-    this.doors = 4;
-  };
-
-  this.say = function() {
-    log.add("I am a " + this.doors + "-door car");
-  };
-}
-
-function Truck() {
-  this.doors = 0;
-
-  this.addParts = function() {
-    this.doors = 2;
-  };
-
-  this.say = function() {
-    log.add("I am a " + this.doors + "-door truck");
-  };
-}
-
-// log helper
-var log = (function () {
-  var log = "";
-  return {
-    add: function (msg) { log += msg + "\n"; },
-    show: function () { console.log(log); log = ""; }
-  }
-})();
-
-function run() {
-  var shop = new Shop();
-  var carBuilder = new CarBuilder();
-  var truckBuilder = new TruckBuilder();
-  var car = shop.construct(carBuilder);
-  var truck = shop.construct(truckBuilder);
-
-  car.say();
-  truck.say();
-
-  log.show();
-}
+// function Shop() {
+//   this.construct = function(builder) {
+//     builder.step1();
+//     builder.step2();
+//     return builder.get();
+//   }
+// }
+//
+// function CarBuilder() {
+//   this.car = null;
+//
+//   this.step1 = function() {
+//     this.car = new Car();
+//   };
+//
+//   this.step2 = function() {
+//     this.car.addParts();
+//   };
+//
+//   this.get = function() {
+//     return this.car;Ω®
+//   };
+// }
+//
+// function TruckBuilder() {
+//   this.truck = null;
+//
+//   this.step1 = function() {
+//     this.truck = new Truck();
+//   };
+//
+//   this.step2 = function() {
+//     this.truck.addParts();
+//   };
+//
+//   this.get = function() {
+//     return this.truck;
+//   };
+// }
+//
+// function Car() {
+//   this.doors = 0;
+//
+//   this.addParts = function() {
+//     this.doors = 4;
+//   };
+//
+//   this.say = function() {
+//     log.add("I am a " + this.doors + "-door car");
+//   };
+// }
+//
+// function Truck() {
+//   this.doors = 0;
+//
+//   this.addParts = function() {
+//     this.doors = 2;
+//   };
+//
+//   this.say = function() {
+//     log.add("I am a " + this.doors + "-door truck");
+//   };
+// }
+//
+// // log helper
+// var log = (function () {
+//   var log = "";
+//   return {
+//     add: function (msg) { log += msg + "\n"; },
+//     show: function () { console.log(log); log = ""; }
+//   }
+// })();
+//
+// function run() {
+//   var shop = new Shop();
+//   var carBuilder = new CarBuilder();
+//   var truckBuilder = new TruckBuilder();
+//   var car = shop.construct(carBuilder);
+//   var truck = shop.construct(truckBuilder);
+//
+//   car.say();
+//   truck.say();
+//
+//   log.show();
+// }
 
