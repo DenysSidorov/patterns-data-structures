836e8e809e6518f6d32939364968420d381a62b8
Denys Sidorov <000scorpions000@gmail.com>
2019-03-26 14:57:28 +0200

    add Adapter pattern

---
+++ patterns/structural/Adapter/adapter.js
@@ -0,0 +1,45 @@
+/**
+ * 'Adapter' pattern allows you create new class with new functionality, but if you want to save
+ * old functionality. In this case you can use Adapter pattern instead of old class.
+ * Method's semantics and invoke approach will be the same like for old class.
+ *
+ * */
+
+/** Using 4/5*/
+
+
+function OldCar(name) {
+    this.name = name;
+    this.run = function () {
+      return `I'm old car - ${this.name}`;
+    }
+}
+
+function AdapterNewCar(name) {
+  this.car = new NewCar(name);
+  this.run = function () {
+    return this.car.fustRun();
+  }
+}
+
+function NewCar(name) {
+    this.name = name;
+    // another method for making car to move
+    this.fustRun = function () {
+      return `I'm new car - ${this.improveCar(this.name)}, brrrr`;
+    }
+    this.improveCar = function (name) {
+      // do some stuff
+      return name.toUpperCase();
+    }
+}
+
+(function userCar() {
+  var car1 = new OldCar('Buick');
+  console.log(car1.run());
+
+  // var car2 = new OldCar('Audi');
+  var car2 = new AdapterNewCar('Audi');
+  console.log(car2.run());
+})()
+
