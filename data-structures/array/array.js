8cc6e94754000ae67450fa28eb5c6f4436003440
Denys Sidorov <000scorpions000@gmail.com>
2021-02-04 01:47:05 +0200

    add complexity to Array data structure

--- data-structures/array/array.js
+++ data-structures/array/array.js
@@ -17,14 +17,18 @@ class Array {
     this.data = {};
   }
 
-  /** This function is used to push an element at the end of the array. */
+  /** This function is used to push an element at the end of the array.
+   * Complexity is - O(1)
+   * */
   push(element) {
     this.data[this.length] = element;
     this.length++;
     return this.length;
   }
 
-  /** It is used to delete an element at the end of the array. */
+  /** It is used to delete an element at the end of the array.
+   * Complexity is - O(1)
+   * */
   pop() {
     if (this.length > 0) {
       let item = this.data[this.length - 1];
@@ -36,7 +40,9 @@ class Array {
     }
   }
 
-  /** This function is used to insert an element at given index. */
+  /** This function is used to insert an element at given index.
+   * Complexity is - O(n)
+   * */
   insertAt(item, index) {
     for (let i = this.length; i >= index; i--) {
       this.data[i] = this.data[i - 1];
@@ -46,7 +52,9 @@ class Array {
     return this.length;
   }
 
-  /** This function is used to remove an element at given index or property in a data object. */
+  /** This function is used to remove an element at given index or property in a data object.
+   * Complexity is - O(n)
+   * */
   deleteAt(index) {
     for (let i = index; i < this.length - 1; i++) {
       this.data[i] = this.data[i + 1];
@@ -56,7 +64,10 @@ class Array {
     return this.data;
   }
 
-  /** It returns the element at given index. */
+  /** It returns the element at given index.
+   * Complexity is - O(1)
+   * */
+
   getElementAtIndex(index) {
     return this.data[index];
   }
